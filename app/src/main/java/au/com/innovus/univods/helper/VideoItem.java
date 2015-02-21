ee2f0327c1c45a572085b7a7f9a9bc4df77c16a8
Jorge Valdivia <valdi.jorge@gmail.com>
2015-02-21 20:16:22 +1030

    xml parsed

---
+++ app/src/main/java/au/com/innovus/univods/helper/VideoItem.java
@@ -0,0 +1,7 @@
+package au.com.innovus.univods.helper;
+
+/**
+ * Created by jorge on 21/02/15.
+ */
+public class VideoItem {
+}
--- app/src/main/java/au/com/innovus/univods/helper/VideoXmlParser.java
+++ app/src/main/java/au/com/innovus/univods/helper/VideoXmlParser.java
@@ -19,28 +19,97 @@ public class VideoXmlParser {
 
     public List parse(String xmlString) throws XmlPullParserException, IOException {
 
+        ArrayList<VideoItem> videoItems = new ArrayList<>();
 
         try {
             XmlPullParser parser = Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(new StringReader(xmlString));
             parser.nextTag();
-            while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+
+            while (parser.next() != XmlPullParser.END_DOCUMENT) {
                 if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    parser.next();
                     continue;
                 }
                 String name = parser.getName();
 
                 if (name.compareToIgnoreCase("item") == 0) {
-                    Log.d(TAG, name);
+                    getVideoItem(parser);
                 }
-                parser.next();
+
+
             }
 
         } finally {
 
         }
-        return new ArrayList();
+        return videoItems;
+    }
+
+    private VideoItem getVideoItem(XmlPullParser parser) throws IOException, XmlPullParserException {
+
+        String title = "";
+        String pubDate = "";
+        String URL = "";
+
+
+        parser.require(XmlPullParser.START_TAG, null, "item");
+
+        while (parser.next() != XmlPullParser.END_DOCUMENT) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+
+            if (name.equals("title")) {
+                title = readTitle(parser);
+                Log.d(TAG, title);
+            }
+            if (name.equals("guid")) {
+                URL = readURL(parser);
+                Log.d(TAG, URL);
+            }
+            if (name.equals("pubDate")) {
+                pubDate = readpubDate(parser);
+                Log.d(TAG, pubDate);
+            }
+
+        }
+
+        return null;
+    }
+
+    // Processes title tags in the feed.
+    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, null, "title");
+        return title;
+    }
+
+    // Processes title tags in the feed.
+    private String readURL(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "guid");
+        String URL = readText(parser);
+        parser.require(XmlPullParser.END_TAG, null, "guid");
+        return URL;
+    }
+
+    // Processes title tags in the feed.
+    private String readpubDate(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "pubDate");
+        String pubDate = readText(parser);
+        parser.require(XmlPullParser.END_TAG, null, "pubDate");
+        return pubDate;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
     }
 }
