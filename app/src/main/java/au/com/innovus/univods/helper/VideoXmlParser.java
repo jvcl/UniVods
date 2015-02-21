0d04d1cd93e901e8013725bb93bc576a4db01f61
Jorge Valdivia <valdi.jorge@gmail.com>
2015-02-22 00:25:44 +1030

    new permission added

--- app/src/main/java/au/com/innovus/univods/helper/VideoXmlParser.java
+++ app/src/main/java/au/com/innovus/univods/helper/VideoXmlParser.java
@@ -5,12 +5,9 @@ import android.util.Xml;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
